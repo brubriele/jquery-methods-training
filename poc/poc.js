66e2311f97860f492ba095b388c8a2bff8679c86
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-08-25 13:12:21 -0300

    [append] Implementa primeiros testes de function appendRow()

--- poc/poc.js
+++ poc/poc.js
@@ -1,5 +1,14 @@
 //Preenchimento da tabela com os dados do form
+function appendRow() {
 
+    $('#submit-form').click(function teste(e) {
+        e.preventDefault();
+        alert('foi')
+        $("<table>")
+            .append("default " + event.type + " prevented")
+            .appendTo("#table");
+    });
+}
 // Cálculo do IMC no insert dos dados
 
 // Para cima, para baixo no limite certo
@@ -9,3 +18,7 @@
 //Edit in modal
 
 //Ordenar por categorias ** hacker edition
+
+(() => {
+    appendRow();
+})();
