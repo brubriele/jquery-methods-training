304b469b0c1619a25bab3e8ef75a1fd1373b6ea6
Bruna Gabriele Paula <bgabriel@everis.com>
2019-08-26 10:25:32 -0300

    [append] - Corrigir para Create Element

--- poc/poc.js
+++ poc/poc.js
@@ -4,15 +4,15 @@ function appendRow() {
     $('#submit-form').click((e) => {
         e.preventDefault();
         // alert('foi');
-        let users = []
+        // let users = []
         let nome = $( "input[type=text][name=nome]" ).val();
         let sobrenome = $( "input[type=text][name=sobrenome]" ).val();
         let peso = Number($( "input[type=text][name=peso]" ).val());
         let altura = Number($( "input[type=text][name=altura]" ).val());
         // Peso dividido pelo quadrado da altura: 70 / 2,89 = 24,22
         let imc = (peso / (altura * altura)) ;
-       
-        users.push(nome)
+        // users.push(nome)
+        let teste = Object.assign({}, appendRow());
 
         
         $("<tbody>")
@@ -29,7 +29,7 @@ function appendRow() {
             <td><i title="Excluir" class="fas fa-trash-alt"></i></td>
         </tr>`)
             .appendTo("#table");
-            console.log(users)
+            console.log(teste);
     });
    
 }
