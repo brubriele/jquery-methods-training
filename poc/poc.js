f6ec92977bcc28c96de2b8b589f12fc5f480909a
Bruna Gabriele Paula <bgabriel@everis.com>
2019-08-29 09:11:44 -0300

    [replace-order] - aborting rebase

--- poc/poc.js
+++ poc/poc.js
@@ -1,7 +1,7 @@
 //Preenchimento da tabela com os dados do form
 // Cálculo do IMC no insert dos dados
 // Peso dividido pelo quadrado da altura: 70 / 2,89 = 24,22
-const id = []
+const id = [1]
 function appendRow() {
     $('#submit-form').click(function generateRow(e) {
         e.preventDefault();
@@ -10,30 +10,31 @@ function appendRow() {
         let peso = Number($("input[type=text][name=peso]").val());
         let altura = Number($("input[type=text][name=altura]").val());
         let imc = (peso / (altura * altura));
-        imc = Number(imc).toFixed(2);   
+        imc = Number(imc).toFixed(2);
         $("<tbody>")
-            .append(`<tr>
-            <th scope="row" data-id="number">${id.length + 1}</th>
-            <td>${nome}</td>
-            <td>${sobrenome}</td>
-            <td>${peso}</td>
-            <td>${altura}</td>
-            <td>${imc}</td>
-            <td><i title="vamos Para cima" class="fas fa-sort-up"></i></td>
-            <td><i title="vamos para baixo" class="fas fa-sort-down"></i></td>
-            <td><i title="vamos para baixo" class="far fa-edit"></i></td>
-            <td><i title="Excluir" class="fas fa-trash-alt"></i></td>
-        </tr>`)
+            .append($("<tr>")
+                .append("<th scope='row' data-id='number'>"+id.length+"</th>")
+                .append("<td>"+nome+"</td>")
+                .append("<td>"+sobrenome+"</td>")
+                .append("<td>"+peso+"</td>")
+                .append("<td>"+altura+"</td>")
+                .append("<td>"+imc+"</td>")
+                .append("<td><i data-key='"+id.length+"' title='vamos Para cima' class='fas fa-sort-up'></i></td>")
+                .append("<td><i title='vamos para baixo' class='fas fa-sort-down'></i></td>")
+                .append("<td><i title='vamos para baixo' class='far fa-edit'></i></td>")
+                .append("<td><i title='Excluir' class='fas fa-trash-alt'></i></td>"))
             .appendTo("#table");
-            // replaceRow();
+            replaceRow ()
     });
+    
+
 }
 
-function replaceRow (e) {
-    e.preventDefault();
-    // let content = 
+function replaceRow () {
     $('.fa-sort-down').click(() => alert('oi'));
+    console.log($("th:nth-child(5) td").css("background-color", "red"))
 }
+
 
 function generateId() {
     $("#submit-form").click(function () {
@@ -42,15 +43,9 @@ function generateId() {
     });
 }
 
-
-
 // Para cima, para baixo no limite certo
-
-
 //Delete linha
-
 //Edit in modal
-
 //Ordenar por categorias ** hacker edition
 
 (() => {
